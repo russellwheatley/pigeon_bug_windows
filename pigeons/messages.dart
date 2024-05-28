4e3579c9f13cf0ebd6c847d10d82b4a84cd8c967
russellwheatley <russellwheatley85@gmail.com>
2024-05-28 12:14:46 +0100

    update pigeon api

--- pigeons/messages.dart
+++ pigeons/messages.dart
@@ -11,8 +11,16 @@ import 'package:pigeon/pigeon.dart';
   cppSourceOut: 'windows/messages.g.cpp',
   copyrightHeader: 'pigeons/copyright.txt',
 ))
+class TestBug {
+  TestBug({
+    required this.anotherString,
+  });
+
+  final String anotherString;
+}
+
 @HostApi(dartHostTestHandler: 'TestUrlLauncherApi')
 abstract class UrlLauncherApi {
-  bool canLaunchUrl(String url);
+  bool canLaunchUrl(String url, TestBug? testBug);
   bool launchUrl(String url);
 }
